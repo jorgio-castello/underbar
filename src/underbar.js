aca0da5828cc2b100246fec16ecf927b8af26f38
Jorge Castello <jorgio.castello@gmail.com>
2020-02-19 14:34:24 -0800

    Add pseudocode for _.memoize
    _.memoize accepts a function
    _.memoize returns the result of that function - _.memoize keeps track
    of parameters it has received and stores the result of said function
    in an object. Reasoning: instead of re-running the function, it can
    simply return the value it previously calculated
    
    Steps:
      1. Declare an empty object called results to keep track of parameters
      received along with results from invoking the function parameter
      2. Return an anonymous function expression
        2a. Convert parameters received into a JSON string
        2b. Check to see if JSON string parameter exists within results obj
        2c. If true: return the value stored at that key
        2d. If false: invoke function on the given parameters, store the
        JSON string along with the function result as a property in the
        object, return the result

--- src/underbar.js
+++ src/underbar.js
@@ -366,6 +366,11 @@
   // already computed the result for the given argument and return that value
   // instead if possible.
   _.memoize = function(func) {
+    //Declare an empty obj called results
+    //Declare an undefined variable called result
+
+
+
   };
 
   // Delays a function for the given number of milliseconds, and then calls
